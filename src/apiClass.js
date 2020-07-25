8f8af0beb3413be5eac82dc25b611e095a64f367
yara <yaramassry1995@gmail.com>
2020-07-26 00:45:54 +0300

    modified on url

--- src/apiClass.js
+++ src/apiClass.js
@@ -15,6 +15,7 @@ export default class apiClass extends React.Component{
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
+        p: "EUR",
         loading : true,
         ratesarr:[],
         fetched: false
@@ -24,17 +25,18 @@ export default class apiClass extends React.Component{
 
 
     async handleSubmit(event){
-        event.preventDefault();
+       event.preventDefault();
         const start = document.getElementById("start");
         const end = document.getElementById("end");
+        const pp =document.getElementById("symbol");
         let  s=start.value;
         let e=end.value;
 
 
-        const url =`https://api.exchangeratesapi.io/history?start_at=${s}&end_at=${e}&symbols=ILS,JPY`;
+        const url =`https://api.exchangeratesapi.io/history?start_at=${s}&end_at=${e}&symbols=${this.state.p}`;
         const response = await fetch(url);
         const d = await response.json();
-        this.setState({ratesarr:d.rates, loading:false, fetched: true});
+        this.setState({ratesarr:d.rates, loading:false, fetched: true , p:pp});
         let arr= d.rates ;
         let messages = [];
 
@@ -66,7 +68,7 @@ export default class apiClass extends React.Component{
 
 render(){
 
-
+ // console.log(p);
     if (this.state.loading)
         return <div style={lstyle}>loading...</div>
         console.log("ratesarr");
@@ -82,8 +84,8 @@ render(){
 
 
             <div>{date}</div>
-             <div>{value.ILS}</div>
-             <div>{value.JPY}</div>
+            
+             
              <div>EUR</div>
 
 
@@ -115,6 +117,15 @@ render(){
           <input
             id="end"
             type="date"
+            name=""
+            required
+          />
+            <br/>
+
+          <h1>Symbol : </h1>
+          <input
+            id="symbol"
+            type="text"
             name=""
             required
           />
